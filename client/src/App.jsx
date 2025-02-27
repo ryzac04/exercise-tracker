1fc729687b1c6623889cfc3664db5c120d48a9ce
ryzac04 <rzc044@gmail.com>
2025-02-27 11:30:45 -0600

    Created navbar and boilerplate for needed components.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom'; 
-import "bootstrap/dist/css/bootstrap.min.css"; 
+import { Routes, Route } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Navbar from './components/Navbar.jsx';
+import ExercisesList from './components/ExercisesList';
+import EditExercises from './components/EditExercises';
+import CreateExercise from './components/CreateExercise';
+import CreateUser from './components/CreateUser';
 
 const App = () => {
   return (
-    <Routes>
+    <>
       <Navbar />
-      <br />
-      <Route path='/' element={<ExercisesList />} />
-      <Route path='/edit/:id' element={<EditExercise />} />
-      <Route path='/create' element={<CreateExercise />} />
-      <Route path='/user' element={<CreateUser />} />
-    </Routes>
+      <Routes>
+        <Route path='/' element={<ExercisesList />} />
+        <Route path='/edit/:id' element={<EditExercises />} />
+        <Route path='/create' element={<CreateExercise />} />
+        <Route path='/user' element={<CreateUser />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ client/src/components/ExercisesList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ExercisesList = () => {
+    return (
+        <div>ExercisesList</div>
+    )
+}
+
+export default ExercisesList
