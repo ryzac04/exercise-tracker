9b74f2740af4c89222c595b93914cc70a408b6ff
ryzac04 <rzc044@gmail.com>
2025-02-27 03:50:08 -0600

    Added router middleware.

--- server/server.js
+++ server/server.js
@@ -2,13 +2,21 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import "dotenv/config";
+import usersRouter from "./routes/users.js";
+import exercisesRouter from "./routes/exercises.js"; 
 
 const app = express();
 const port = process.env.PORT || 5000;
 const URI = process.env.ATLAS_URI;
 
+// Middleware for handling CORS Policy
 app.use(cors());
+// Middleware for parsing request body
 app.use(express.json());
+
+// Routes middleware
+app.use("/exercises", exercisesRouter);
+app.use("/users", usersRouter); 
 
 mongoose
     .connect(URI)
