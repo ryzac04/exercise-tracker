cebb8db1b633e02db633cef6bdacddf15f40a40f
ryzac04 <rzc044@gmail.com>
2025-02-27 03:48:03 -0600

    Created exercise model.

--- server/models/exercise.model.js
+++ server/models/exercise.model.js
@@ -1,1 +1,15 @@
 import mongoose from "mongoose";
+
+const exerciseSchema = mongoose.Schema(
+    {
+        username: { type: String, required: true },
+        description: { type: String, required: true },
+        duration: { type: Number, required: true },
+        date: {type: Date, required: true}
+    }, 
+    {
+        timestamps: true
+    }
+);
+
+export const Exercise = mongoose.model("Exercise", exerciseSchema);
